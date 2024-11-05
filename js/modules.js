3ab2c96cdb381ab1ca0fe8c354d648feaaf2915b
gzkakeru-onuki <gzdevonuki@gmail.com>
2024-11-05 10:28:28 +0900

    first commit

---
+++ js/modules.js
@@ -0,0 +1,15 @@
+
+const API_KEY = "AIzaSyDhGwijNDytWNMP2Igxpx6X0iY0PMFfNWQ";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC_mBpqTOdQvh9v9NbEiaFriWAc2oEpYNw",
+    authDomain: "chatniki.firebaseapp.com",
+    databaseURL: "https://chatniki-default-rtdb.firebaseio.com",
+    projectId: "chatniki",
+    storageBucket: "chatniki.firebasestorage.app",
+    messagingSenderId: "744392928982",
+    appId: "1:744392928982:web:dd68ed7c377cc04b0d6377"
+};
+export {API_KEY};
+export {firebaseConfig};
